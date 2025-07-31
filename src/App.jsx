1ed3e8c9cacc2b6f8f53239370d9939cba0ba436
swkil <0826monica@naver.com>
2025-07-31 11:38:41 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Counter from "./components/Counter";
+import InputText from "./components/InputText";
+import ItemAdd from "./components/ItemAdd";
 
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      <Counter />
+      <ItemAdd />
     </div>
   );
 };
